3fb6719266bd96867840bc212ae9041861e98413
mbliefer <mblief@gmail.com>
2023-03-06 12:17:03 -0600

    change functions

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const output = document.querySelector('.output');
 const clear = document.querySelector('#clear');
 const buttons = document.querySelectorAll(".buttons");
 const calculator = document.querySelector(".calculator");
-let num;
+let firstNum = '';
+let secondNum = '';
 let operation;
 let total;
+let shouldResetScreen = false;
 
 // enter number and display
 // enter operator and display either 1st number or total until new number is entered
 // 1st number is total OR 1st number(operand)second number is total
 
-const getNumberKey = (e) => {
+const handleNumberKey = (e) => {
     numbers.forEach((number) => {
         if (number.value === e.key) {
             number.click();
@@ -20,58 +22,101 @@ const getNumberKey = (e) => {
             number.addEventListener('transitionend', removeTransition);
         };
     });
-}
-
-const getOperatorKey = (e) => {
-    operation = e.key;
-    total = num;
     operators.forEach((operator) => {
-        if (operator.value == e.key) {
-            console.log(operator.id)
-            console.log(operator.value);
-        };
-    });
-    if (operation == "/" || operation == "*" || operation == "-" || operation == "+") {
-        console.log(operation);
-        total = num;
-    }
-    if (operation == "Enter") {
-        e.preventDefault();
-        operate(operation, total, num);
-    } else {
-        return;
-    }
+        if (operator.value === e.key) {
+            operator.click();
+        }
+    })
 }
 
-const getOperatorClick = (e) => {
-    console.log(e.target.id);
+    // operators.forEach((operator) => {
+    //     if (operator.value == e.key) {
+    //         console.log(operator.id)
+    //         console.log(operation);
+    //         operate(operation, firstNum, secondNum);
+    //     };
+    // });
 
-    startNewNum();
+const handleOperatorKey = (e) => {
+    if (firstNum === '') {
+        firstNum = secondNum;
+        shouldResetScreen = true;
+        console.log(operation);
+        operation = e.key;
+    }
+    if (operation === "Enter" || operation === "=") {
+        e.preventDefault();
+        operate(operation, total, firstNum);
+    } else {
+        console.log(operation)
+        total = operate(operation, firstNum, secondNum);
+        displayNewNum();    
+    }
+    operators.forEach((operator) => {
+            if (operator.value == e.key && firstNum != '') {
+                operation = e.key;
+                console.log(operation);
+            };
+        });
+}
+
+const handleOperatorClick = (e) => {
+    if (firstNum === '') {
+        firstNum = secondNum;
+        shouldResetScreen = true;
+        console.log(operation);
+    }
+    if (operation === "Enter" || operation === "=") {
+        e.preventDefault();
+        total = operate(operation, firstNum, secondNum);
+        displayNewNum();
+        return;
+    } else {
+    total = operate(operation, firstNum, secondNum);
+    displayNewNum();
+    }
+    operation = e.target.value;
+    console.log(operation);
 }
 
 const clearCalculator = () => {
     output.textContent = 0;
-    num = 0;
+    firstNum = 0;
     total = 0;
 }
 
+
+function resetScreen() {
+    output.textContent = "";
+    shouldResetScreen = false;
+}
+
+function displayNewNum() {
+    output.textContent = total;
+    firstNum = total;
+    shouldResetScreen = true;
+};
+
 function printNumber(number) {
-    if (output.textContent == 0) output.textContent = "";
+    if (output.textContent == 0 || shouldResetScreen) resetScreen();
     if (output.textContent.length < 9) {
         output.textContent += number;
-        num = parseFloat(output.textContent);
+        secondNum = parseFloat(output.textContent);
     }
     else {
-        console.log("hi");
-        console.log(calculator.outerHTML);
         calculator.classList.add("shake");
-        calculator.addEventListener('transitionend', () => {
-            calculator.classList.remove("shake")
-        });
+        removeShake();
     }
 }
 
+function removeShake() {
+    calculator.addEventListener('transitionend', () => {
+        calculator.classList.remove("shake")
+    });
+}
+
 function removeTransition() {
+    console.log("active");
     this.classList.remove("active");
 }
 
@@ -81,33 +126,24 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 function operate(operator, a, b) {
-    if (operator === "+") console.log(add(a, b));
+    if (operator === "+") return(add(a, b));
     if (operator === "-") return subtract(a, b);
     if (operator === "*") return multiply(a, b);
     if (operator === "/") return divide(a, b);
 };
 
-function startNewNum() {
-    output.textContent = 0;
-    num = output.textContent;
-};
-
-function getTotal() {
-
-}
-
 // click a number on the calculator and add to display
 numbers.forEach((number) => {
-    number.addEventListener('click', () => printNumber(number.value))
+    number.addEventListener('click', () => printNumber(number.value));
 });
 // click on operator
 operators.forEach((operator) => {
-    operator.addEventListener('click', getOperatorClick);
+    operator.addEventListener('click', handleOperatorClick);
 });
 
 window.addEventListener('keydown', (e) => {
-    getNumberKey(e);
-    getOperatorKey(e);
+    handleNumberKey(e);
+    // handleOperatorKey(e);
 });
 
 clear.addEventListener('click', clearCalculator);
